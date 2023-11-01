e1066b6d49c423b17061a7db9e91a789f6ecdb6e
AKKATIYAR22 <106366138+AKKATIYAR22@users.noreply.github.com>
2023-11-02 02:01:15 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,109 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+import datetime
+import smtplib
+import datetime
+import csv 
+import pandas as pd
+
+
+URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+page = requests.get(URL, headers=headers)
+
+soup1 = BeautifulSoup(page.content, "html.parser")
+
+soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+title = soup2.find(id='productTitle').get_text()
+
+price = soup2.find(id='priceblock_ourprice').get_text()
+
+
+print(title)
+print(price)
+
+price = price.strip()[1:]
+title = title.strip()
+
+print(title)
+print(price)
+
+today = datetime.date.today()
+
+print(today)
+
+
+header = ['Title', 'Price', 'Date']
+data = [title, price, today]
+
+
+with open('AmazonWebScraperDataset.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerow(data)
+
+
+df = pd.read_csv(r'C:\Users\alexf\AmazonWebScraperDataset.csv')
+
+print(df)
+
+with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(data)
+
+def check_price():
+
+    URL = 'https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data%2Banalyst%2Btshirt&qid=1626655184&sr=8-3&customId=B0752XJYNL&th=1'
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    page = requests.get(URL, headers=headers)
+
+    soup1 = BeautifulSoup(page.content, "html.parser")
+
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+    title = soup2.find(id='productTitle').get_text()
+
+    price = soup2.find(id='priceblock_ourprice').get_text()
+
+    price = price.strip()[1:]
+    title = title.strip()
+
+
+    with open('AmazonWebScraperDataset.csv', 'a+', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
+while(True):
+    check_price()
+    time.sleep(86400)
+
+# import pandas as pd
+
+# df = pd.read_csv(r'C:\Users\alexf\AmazonWebScraperDataset.csv')
+
+# print(df)
+
+def send_mail():
+    server = smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.ehlo()
+    #server.starttls()
+    server.ehlo()
+    server.login('AlexTheAnalyst95@gmail.com','xxxxxxxxxxxxxx')
+    
+    subject = "The Shirt you want is below $15! Now is your chance to buy!"
+    body = "Alex, This is the moment we have been waiting for. Now is your chance to pick up the shirt of your dreams. Don't mess it up! Link here: https://www.amazon.com/Funny-Data-Systems-Business-Analyst/dp/B07FNW9FGJ/ref=sr_1_3?dchild=1&keywords=data+analyst+tshirt&qid=1626655184&sr=8-3"
+   
+    msg = f"Subject: {subject}\n\n{body}"
+    
+    server.sendmail(
+        'AlexTheAnalyst95@gmail.com',
+        msg
+     
+    )
